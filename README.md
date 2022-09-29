96c211cffed45888baa0d744b4df3ad3573f3fe4
CalwinTan <twpin29@gmail.com>
2022-09-29 18:38:05 +0800

    sync

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # portfolio
+
+[reference example]
+/*!
+ * ScrollTrigger 3.3.4
+ * https://greensock.com
+ * 
+ * @license Copyright 2020, GreenSock. All rights reserved.
+ * Subject to the terms at https://greensock.com/standard-license or for Club GreenSock members, the agreement issued with that membership.
+ * @author: Jack Doyle, jack@greensock.com
+ */
+
+[搜尋codepen關鍵字]
+-SVG
+-Canvas
+-Gallery
+-Animation
--- test.js
+++ test.js
@@ -1,77 +1,440 @@
-Vue.config.devtools = true;
+gsap.registerPlugin(ScrollTrigger);
+let speed = 100;
 
-Vue.component('card', {
-    template: `
-    <div class="card-wrap"
-      @mousemove="handleMouseMove"
-      @mouseenter="handleMouseEnter"
-      @mouseleave="handleMouseLeave"
-      ref="card">
-      <div class="card"
-        :style="cardStyle">
-        <div class="card-bg" :style="[cardBgTransform, cardBgImage]"></div>
-        <div class="card-info">
-          <slot name="header"></slot>
-          <slot name="content"></slot>
-        </div>
-      </div>
-    </div>`,
-    mounted() {
-        this.width = this.$refs.card.offsetWidth;
-        this.height = this.$refs.card.offsetHeight;
-    },
-    props: ['dataImage'],
-    data: () => ({
-        width: 0,
-        height: 0,
-        mouseX: 0,
-        mouseY: 0,
-        mouseLeaveDelay: null
-    }),
-    computed: {
-        mousePX() {
-            return this.mouseX / this.width;
-        },
-        mousePY() {
-            return this.mouseY / this.height;
-        },
-        cardStyle() {
-            const rX = this.mousePX * 30;
-            const rY = this.mousePY * -30;
-            return {
-                transform: `rotateY(${rX}deg) rotateX(${rY}deg)`
-            };
-        },
-        cardBgTransform() {
-            const tX = this.mousePX * -40;
-            const tY = this.mousePY * -40;
-            return {
-                transform: `translateX(${tX}px) translateY(${tY}px)`
-            }
-        },
-        cardBgImage() {
-            return {
-                backgroundImage: `url(${this.dataImage})`
-            }
-        }
-    },
-    methods: {
-        handleMouseMove(e) {
-            this.mouseX = e.pageX - this.$refs.card.offsetLeft - this.width / 2;
-            this.mouseY = e.pageY - this.$refs.card.offsetTop - this.height / 2;
-        },
-        handleMouseEnter() {
-            clearTimeout(this.mouseLeaveDelay);
-        },
-        handleMouseLeave() {
-            this.mouseLeaveDelay = setTimeout(() => {
-                this.mouseX = 0;
-                this.mouseY = 0;
-            }, 1000);
-        }
-    }
+/*  SCENE 1 */
+let scene1 = gsap.timeline();
+ScrollTrigger.create({
+    animation: scene1,
+    trigger: ".scrollElement",
+    start: "top top",
+    end: "45% 100%",
+    scrub: 3,
 });
 
-const app = new Vue({
-    el: '#app'
+// hills animation 
+scene1.to("#h1-1", {
+    y: 3 * speed,
+    x: 1 * speed,
+    scale: 0.9,
+    ease: "power1.in"
+}, 0)
+scene1.to("#h1-2", {
+    y: 2.6 * speed,
+    x: -0.6 * speed,
+    ease: "power1.in"
+}, 0)
+scene1.to("#h1-3", {
+    y: 1.7 * speed,
+    x: 1.2 * speed
+}, 0.03)
+scene1.to("#h1-4", {
+    y: 3 * speed,
+    x: 1 * speed
+}, 0.03)
+scene1.to("#h1-5", {
+    y: 2 * speed,
+    x: 1 * speed
+}, 0.03)
+scene1.to("#h1-6", {
+    y: 2.3 * speed,
+    x: -2.5 * speed
+}, 0)
+scene1.to("#h1-7", {
+    y: 5 * speed,
+    x: 1.6 * speed
+}, 0)
+scene1.to("#h1-8", {
+    y: 3.5 * speed,
+    x: 0.2 * speed
+}, 0)
+scene1.to("#h1-9", {
+    y: 3.5 * speed,
+    x: -0.2 * speed
+}, 0)
+
+//animate text
+scene1.to("#info", {
+    y: 8 * speed
+}, 0)
+
+
+
+/*   Bird   */
+gsap.fromTo("#bird", {
+    opacity: 1
+}, {
+    y: -250,
+    x: 800,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".scrollElement",
+        start: "15% top",
+        end: "60% 100%",
+        scrub: 4,
+        onEnter: function () {
+            gsap.to("#bird", {
+                scaleX: 1,
+                rotation: 0
+            })
+        },
+        onLeave: function () {
+            gsap.to("#bird", {
+                scaleX: -1,
+                rotation: -15
+            })
+        },
+    }
+})
+
+
+/* Clouds  */
+let clouds = gsap.timeline();
+ScrollTrigger.create({
+    animation: clouds,
+    trigger: ".scrollElement",
+    start: "top top",
+    end: "70% 100%",
+    scrub: 1,
+});
+
+clouds.to("#cloud1", {
+    x: 500
+}, 0)
+clouds.to("#cloud2", {
+    x: 1000
+}, 0)
+clouds.to("#cloud3", {
+    x: -1000
+}, 0)
+clouds.to("#cloud4", {
+    x: -700,
+    y: 25
+}, 0)
+
+
+
+/* Sun motion Animation  */
+let sun = gsap.timeline();
+ScrollTrigger.create({
+    animation: sun,
+    trigger: ".scrollElement",
+    start: "top top",
+    end: "2200 100%",
+    scrub: 1,
+});
+
+//sun motion 
+sun.to("#bg_grad", {
+    attr: {
+        cy: "330"
+    }
+}, 0.00)
+
+//bg change
+sun.to("#sun", {
+    attr: {
+        offset: "0.15"
+    }
+}, 0.00)
+sun.to("#bg_grad stop:nth-child(2)", {
+    attr: {
+        offset: "0.15"
+    }
+}, 0.00)
+sun.to("#bg_grad stop:nth-child(3)", {
+    attr: {
+        offset: "0.18"
+    }
+}, 0.00)
+sun.to("#bg_grad stop:nth-child(4)", {
+    attr: {
+        offset: "0.25"
+    }
+}, 0.00)
+sun.to("#bg_grad stop:nth-child(5)", {
+    attr: {
+        offset: "0.46"
+    }
+}, 0.00)
+sun.to("#bg_grad stop:nth-child(6)", {
+    attr: {
+        "stop-color": "#FF9171"
+    }
+}, 0)
+
+
+
+/*   SCENE 2  */
+let scene2 = gsap.timeline();
+ScrollTrigger.create({
+    animation: scene2,
+    trigger: ".scrollElement",
+    start: "15% top",
+    end: "40% 100%",
+    scrub: 4,
+});
+
+scene2.fromTo("#h2-1", {
+    y: 500,
+    opacity: 0
+}, {
+    y: 0,
+    opacity: 1
+}, 0)
+scene2.fromTo("#h2-2", {
+    y: 500
+}, {
+    y: 0
+}, 0.1)
+scene2.fromTo("#h2-3", {
+    y: 700
+}, {
+    y: 0
+}, 0.1)
+scene2.fromTo("#h2-4", {
+    y: 700
+}, {
+    y: 0
+}, 0.2)
+scene2.fromTo("#h2-5", {
+    y: 800
+}, {
+    y: 0
+}, 0.3)
+scene2.fromTo("#h2-6", {
+    y: 900
+}, {
+    y: 0
+}, 0.3)
+
+
+
+/* Bats */
+gsap.fromTo("#bats", {
+    opacity: 1,
+    y: 400,
+    scale: 0
+}, {
+    y: 120,
+    scale: 0.8,
+    transformOrigin: "50% 50%",
+    ease: "power3.out",
+    scrollTrigger: {
+        trigger: ".scrollElement",
+        start: "40% top",
+        end: "70% 100%",
+        scrub: 3,
+        onEnter: function () {
+            gsap.utils.toArray("#bats path").forEach((item, i) => {
+                gsap.to(item, {
+                    scaleX: 0.5,
+                    yoyo: true,
+                    repeat: 11,
+                    duration: 0.15,
+                    delay: 0.7 + (i / 10),
+                    transformOrigin: "50% 50%"
+                })
+            });
+            gsap.set("#bats", {
+                opacity: 1
+            })
+        },
+        onLeave: function () {
+            gsap.to("#bats", {
+                opacity: 0,
+                delay: 2
+            })
+        },
+    }
+})
+
+
+/* Sun increase */
+let sun2 = gsap.timeline();
+ScrollTrigger.create({
+    animation: sun2,
+    trigger: ".scrollElement",
+    start: "2200 top",
+    end: "5000 100%",
+    scrub: 1,
+});
+
+sun2.to("#sun", {
+    attr: {
+        offset: "0.6"
+    }
+}, 0)
+sun2.to("#bg_grad stop:nth-child(2)", {
+    attr: {
+        offset: "0.7"
+    }
+}, 0)
+sun2.to("#sun", {
+    attr: {
+        "stop-color": "#ffff00"
+    }
+}, 0)
+sun2.to("#lg4 stop:nth-child(1)", {
+    attr: {
+        "stop-color": "#623951"
+    }
+}, 0)
+sun2.to("#lg4 stop:nth-child(2)", {
+    attr: {
+        "stop-color": "#261F36"
+    }
+}, 0)
+sun2.to("#bg_grad stop:nth-child(6)", {
+    attr: {
+        "stop-color": "#45224A"
+    }
+}, 0)
+
+
+
+/* Transition (from Scene2 to Scene3) */
+gsap.set("#scene3", {
+    y: 580,
+    visibility: "visible"
+})
+let sceneTransition = gsap.timeline();
+ScrollTrigger.create({
+    animation: sceneTransition,
+    trigger: ".scrollElement",
+    start: "70% top",
+    end: "bottom 100%",
+    scrub: 3,
+});
+
+sceneTransition.to("#h2-1", {
+    y: -680,
+    scale: 1.5,
+    transformOrigin: "50% 50%"
+}, 0)
+sceneTransition.to("#bg_grad", {
+    attr: {
+        cy: "-80"
+    }
+}, 0.00)
+sceneTransition.to("#bg2", {
+    y: 0
+}, 0)
+
+
+
+/* Scene 3 */
+let scene3 = gsap.timeline();
+ScrollTrigger.create({
+    animation: scene3,
+    trigger: ".scrollElement",
+    start: "80% 50%",
+    end: "bottom 100%",
+    scrub: 3,
+});
+
+//Hills motion
+scene3.fromTo("#h3-1", {
+    y: 300
+}, {
+    y: -550
+}, 0)
+scene3.fromTo("#h3-2", {
+    y: 800
+}, {
+    y: -550
+}, 0.03)
+scene3.fromTo("#h3-3", {
+    y: 600
+}, {
+    y: -550
+}, 0.06)
+scene3.fromTo("#h3-4", {
+    y: 800
+}, {
+    y: -550
+}, 0.09)
+scene3.fromTo("#h3-5", {
+    y: 1000
+}, {
+    y: -550
+}, 0.12)
+
+//stars
+scene3.fromTo("#stars", {
+    opacity: 0
+}, {
+    opacity: 0.5,
+    y: -500
+}, 0)
+
+// Scroll Back text
+scene3.fromTo("#arrow2", {
+    opacity: 0
+}, {
+    opacity: 0.7,
+    y: -710
+}, 0.25)
+scene3.fromTo("#text2", {
+    opacity: 0
+}, {
+    opacity: 0.7,
+    y: -710
+}, 0.3)
+
+//gradient value change
+scene3.to("#bg2-grad", {
+    attr: {
+        cy: 600
+    }
+}, 0)
+scene3.to("#bg2-grad", {
+    attr: {
+        r: 500
+    }
+}, 0)
+
+
+/*   falling star   */
+gsap.to("#fstar", {
+    x: -700,
+    y: -250,
+    ease: "power4.out",
+    scrollTrigger: {
+        trigger: ".scrollElement",
+        start: "4000 top",
+        end: "6000 100%",
+        scrub: 5,
+        onEnter: function () {
+            gsap.set("#fstar", {
+                opacity: 1
+            })
+        },
+        onLeave: function () {
+            gsap.set("#fstar", {
+                opacity: 0
+            })
+        },
+    }
+})
+
+
+//reset scrollbar position after refresh
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
+
+
+let fullscreen;
+let fsEnter = document.getElementById('fullscr');
+fsEnter.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (!fullscreen) {
+        fullscreen = true;
+        document.documentElement.requestFullscreen();
+        fsEnter.innerHTML = "Exit Fullscreen";
+    } else {
+        fullscreen = false;
+        document.exitFullscreen();
+        fsEnter.innerHTML = "Go Fullscreen";
+    }
 });
